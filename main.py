1f543c4c3a7fbd4d0855fb9fb1af506d48419d85
Aditya Attrish <aditayaattrish@gmail.com>
2025-10-21 17:21:21 +0530

    add vercel.json

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from app.__init__ import create_app
-#from routes import app
 from app.extensions import db
 
 app = create_app()
 
+# Initialize database tables
+with app.app_context():
+    db.create_all()
+
+# This is for Vercel
 if __name__ == '__main__':
-    with app.app_context():
-    	db.create_all()
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    app.run()
